14e2c9297edf45ba9aa81824d909fb3afcc8341a
kashish567 <kashishbhanushali7@gmail.com>
2024-07-28 13:55:11 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config'
+import { studentModule } from './student/student.module';
+import { redisStore } from 'cache-manager-redis-yet';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const redisUrl = configService.get<string>('redis.url');
+        return {
+          store: await redisStore({
+            url: redisUrl,
+          }),
+        };
+      },
+      inject: [ConfigService],
+    }),
+    studentModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/student/student.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, UseInterceptors } from "@nestjs/common";
+import { studentService } from "./student.service";
+import { CacheInterceptor, CacheTTL } from "@nestjs/cache-manager";
+
+
+@UseInterceptors(CacheInterceptor)
+@CacheTTL(6*1000)
+@Controller('student')
+export class StudentController {    
+    constructor(private readonly studentService: studentService) {}
+
+    @Get()
+    async getStudents() {
+        console.log("Inside controller")    
+        return this.studentService.getStudents()
+    }
+}
